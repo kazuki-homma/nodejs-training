3a8e293f11a417b5d21c8caa79b9f8032563d058
kazuki-h <kazuki-h@weseek.co.jp>
2021-02-21 16:25:09 +0900

    express init

--- ref/ref.js
+++
@@ -1,7 +0,0 @@
-// https://qiita.com/Gatz/items/55325aaa495a1f62c177
-
-// MIME type
-// http://www.tohoho-web.com/wwwxx015.htm
-
-// ノンブロッキングI/Oモデル
-// https://blog.takanabe.tokyo/2015/03/%E3%83%8E%E3%83%B3%E3%83%96%E3%83%AD%E3%83%83%E3%82%AD%E3%83%B3%E3%82%B0i/o%E3%81%A8%E9%9D%9E%E5%90%8C%E6%9C%9Fi/o%E3%81%AE%E9%81%95%E3%81%84%E3%82%92%E7%90%86%E8%A7%A3%E3%81%99%E3%82%8B/
